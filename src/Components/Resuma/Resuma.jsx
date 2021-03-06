0df440b6926b20159c823fa6f851bf6001082d03
NileshGhimire <nileshghimire74@gmail..com>
2021-03-06 12:19:43 +0545

    trying to solve error

--- src/Components/Resuma/Resuma.jsx
+++ src/Components/Resuma/Resuma.jsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
     padding: "1rem",
     margin: "0 auto",
     "&:before": {
-      content: "''",
+      content: "'",
       position: "absolute",
       height: "100%",
       border: "1px solid tan",
@@ -20,86 +20,10 @@ const useStyles = makeStyles((theme) => ({
       top: 0,
     },
     "&:after": {
-      content: "''",
+      content: "'",
       display: "table",
       clear: "both",
     },
-    [theme.breakpoints.up("md")]: {
-      padding: "2rem",
-      "&:before": {
-        left: "calc(50% - 1px)",
-        right: "auto",
-      },
-    },
-  },
-  timeLineItem: {
-    padding: "1rem",
-    borderBottom: "2px solid tan",
-    position: "relative",
-    margin: "1rem 3rem 1rem 1rem",
-    clear: "both",
-    "&:after": {
-      content: "''",
-      position: "absolute",
-    },
-    "&:before": {
-      content: "''",
-      position: "absolute",
-      right: "-0.625rem",
-      top: "calc(50% - 5px)",
-      borderStyle: "solid",
-      borderColor: "tomato tomato transparent transparent",
-      borderWidth: "0.625rem",
-      transform: "rotate(45deg)",
-    },
-    [theme.breakpoints.up("md")]: {
-      width: "44%",
-      margin: "1rem",
-      "&:nth-of-type(2n)": {
-        float: "right",
-        margin: "1rem",
-        borderColor: "tan",
-      },
-      "&:nth-of-type(2n):before": {
-        right: "auto",
-        left: "-0.625rem",
-        borderColor: "transparent transparent tomato tomato",
-      },
-    },
-  },
-  timeLineYear: {
-    textAlign: "center",
-    maxWidth: "9.375rem",
-    margin: "0 3rem 0 auto",
-    fontSize: "1.8rem",
-    background: "tomato",
-    color: "white",
-    lineHeight: 1,
-    padding: "0.5rem 0 1rem",
-    "&:before": {
-      display: "none",
-    },
-    [theme.breakpoints.up("md")]: {
-      textAlign: "center",
-      margin: "0 auto",
-      "&:nth-of-type(2n)": {
-        float: "none",
-        margin: "0 auto",
-      },
-      "&:nth-of-type(2n):before": {
-        display: "none",
-      },
-    },
-  },
-  heading: {
-    color: "grey",
-    padding: "3rem 0",
-    textTransform: "uppercase",
-  },
-  subHeading: {
-    color: "white",
-    padding: "0",
-    textTransform: "uppercase",
   },
 }));
 const Resuma = () => {
@@ -108,50 +32,12 @@ const Resuma = () => {
     <>
       <Navbar></Navbar>
       <Box component="header" className={classes.mainContainer}>
-        <Typography variant="h4" align="center" className={classes.heading}>
+        <Typography varient="h4" align="center">
           Working Experience
         </Typography>
 
-        <Box component="div" className={classes.timeLine}>
-          <Typography
-            variant="h2"
-            className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-          >
-            2017
-          </Typography>
-
-          <Box component="div" className={classes.timeLineItem}>
-            <Typography variant="h5" className={classes.subHeading}>
-              Web Design
-            </Typography>
-            <Typography variant="body1" style={{ color: "tomato" }}>
-              1 year self working experience while in university
-            </Typography>
-            <Typography variant="subtitle1" style={{ color: "tan" }}>
-              Web design and development was module were learned about
-              fundmental of Web while studying in Campus.
-            </Typography>
-          </Box>
-
-          <Typography
-            variant="h2"
-            className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-          >
-            2018
-          </Typography>
-
-          <Box component="div" className={classes.timeLineItem}>
-            <Typography variant="h5" className={classes.subHeading}>
-              ASp.net
-            </Typography>
-            <Typography variant="body1" style={{ color: "tomato" }}>
-              6 month self working experience while in campus
-            </Typography>
-            <Typography variant="subtitle1" style={{ color: "tan" }}>
-              Web design and development was module were learned about
-              fundmental of Web while studying in Campus.
-            </Typography>
-          </Box>
+        <Box className={classes.timeLine}>
+          <Typography varient="h2">2020</Typography>
         </Box>
       </Box>
     </>
