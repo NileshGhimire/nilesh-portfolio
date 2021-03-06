a3cac1b79f28cab8dab6755cbc8cf114c68911a5
NileshGhimire <nileshghimire74@gmail..com>
2021-03-06 12:11:43 +0545

    trying to solve error

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import style from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  Typography,
+  ListItem,
+  List,
+  Avatar,
+  Divider,
+  ListItemText,
+  ListItemIcon,
+} from "@material-ui/core";
+import { Home } from "@material-ui/icons";
+import nilesh from "../../images/276A1842.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import { menuIcons } from "../../storage/NavData";
+import MobilRightMenuSlider from "@material-ui/core/Drawer";
+// CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  menuSlideContainer: {
+    width: 250,
+    background: "darkslategrey",
+    height: "100%",
+  },
+  logoAvatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: "5rem",
+    height: "5rem",
+  },
+  listData: {
+    color: "tan",
+  },
+}));
+
+const Navbar = () => {
+  const [state, setState] = useState({ right: false });
+
+  const toggleSlider = (slider, open) => () => {
+    setState({ ...state, [slider]: open });
+  };
+  const classes = useStyles();
+  const Sidebar = (slider) => (
+    <Box
+      component="div"
+      className={classes.menuSlideContainer}
+      onClick={toggleSlider(slider, false)}
+    >
+      <Avatar src={nilesh} alt="Nilesh" className={classes.logoAvatar}></Avatar>
+      <Divider />
+      <List>
+        {menuIcons.map((item, key) => (
+          <ListItem button key={key} component={Link} to={item.listPath}>
+            <ListItemIcon className={classes.listData}>
+              {item.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listData}
+              primary={item.listText}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <Box component="nav">
+        <AppBar
+          position="static"
+          style={{ background: "darkslategrey", width: "100%" }}
+        >
+          <Toolbar>
+            <IconButton onClick={toggleSlider("right", true)}>
+              <Home style={{ color: "tan" }}></Home>
+            </IconButton>
+            <Typography varient="h3" style={{ color: "tan" }}>
+              Nilesh Ghimire
+            </Typography>
+
+            <MobilRightMenuSlider
+              open={state.right}
+              anchor="right"
+              onClose={toggleSlider("right", false)}
+            >
+              {Sidebar("right")}
+            </MobilRightMenuSlider>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
